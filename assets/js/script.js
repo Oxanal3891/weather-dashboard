613599f0587331ea50a3818f4e9ad89ad39438b4
Oxana <oxanal3891@gmail.com>
2023-12-19 18:02:17 +0000

    Added a fetch API function

---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+//Declare variables
+var searchBtn = $('#search-button');
+var clearBtn = $('#clear');
+var input = $('#search-input');
+var city = $('#city');
+var temperature = $('#temperature');
+var wind = $('#wind');
+var humidity = $('#humidity');
+var index = $('#index');
+var citiesPrev = $('.input-group-append');
+
+
+
+//Fetch API on button click
+
+$searchBtn.on('click', function (event) {
+    console.log(input.value);
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=ec96c3d6509b8a012ba07a86b8f2719b')
+        .then(response => response.json())
+        .then(data => {
+
+            var cityValue = data['name'];
+            var temperatureerValue = data['main']['temp'];
+            var windValue = data['wind']['speed'];
+            var humidityValue = data['main']['humidity'];
+            var ImgValue = data.weather[0].icon;
+
+
+            city.innerHTML = (cityValue + " ");
+            var converttemperature = Math.trunc(1.8 * (temperatureerValue - 273) + 32);
+            temperature.innerHTML = ("temperatureerature: " + converttemperature + " °F");
+            wind.innerHTML = ("wind: " + windValue + " mph");
+            humidity.innerHTML = ("humidity: " + humidityValue);
+
+
+            addicon.src = "http://openweathermap.org/img/wn/" + ImgValue + ".png";
+
+            document.getElementById('index').append(addicon);
+
+            historyCities.push(cityValue);
+            localStorage.setItem('saved', JSON.stringify(historyCities));
+            addEntry();
+        })
+
+        .catch(err => alert("Wrong city name"))
+
+    // fetch request to get 5-day forecast
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + input.value + '&units=imperial&appid=2e9ee85817b120b2f084253eb364f16d')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            var icon1 = 'https://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png';
+            $('#icon1').attr('src', icon1);
+            document.getElementById("temperature1").innerHTML = 'temperature: ' + Number(data.list[0].main.temp).toFixed(0) + "°F";
+            document.getElementById("wind1").innerHTML = 'wind: ' + Number(data.list[0].wind.speed) + " Mph";
+            document.getElementById("humidity1").innerHTML = 'humidity: ' + Number(data.list[0].main.humidity);
+            document.getElementById("date1").innerHTML = (data.list[0].dt_txt);
+
+            var icon2 = 'https://openweathermap.org/img/w/' + data.list[8].weather[0].icon + '.png';
+            $('#icon2').attr('src', icon2);
+            document.getElementById("temperature2").innerHTML = 'temperature: ' + Number(data.list[8].main.temp).toFixed(0) + "°F";
+            document.getElementById("wind2").innerHTML = 'wind: ' + Number(data.list[8].wind.speed) + " Mph";
+            document.getElementById("humidity2").innerHTML = 'humidity: ' + Number(data.list[8].main.humidity);
+            document.getElementById("date2").innerHTML = (data.list[8].dt_txt);
+
+            var icon3 = 'https://openweathermap.org/img/w/' + data.list[16].weather[0].icon + '.png';
+            $('#icon3').attr('src', icon3);
+            document.getElementById("temperature3").innerHTML = 'temperature: ' + Number(data.list[16].main.temp).toFixed(0) + "°F";
+            document.getElementById("wind3").innerHTML = 'wind: ' + Number(data.list[16].wind.speed) + " Mph";
+            document.getElementById("humidity3").innerHTML = 'humidity: ' + Number(data.list[16].main.humidity);
+            document.getElementById("date3").innerHTML = (data.list[16].dt_txt);
+
+            var icon4 = 'https://openweathermap.org/img/w/' + data.list[24].weather[0].icon + '.png';
+            $('#icon4').attr('src', icon4);
+            document.getElementById("temperature4").innerHTML = 'temperature: ' + Number(data.list[24].main.temp).toFixed(0) + "°F";
+            document.getElementById("wind4").innerHTML = 'wind: ' + Number(data.list[24].wind.speed) + " Mph";
+            document.getElementById("humidity4").innerHTML = 'humidity: ' + Number(data.list[24].main.humidity);
+            document.getElementById("date4").innerHTML = (data.list[24].dt_txt);
+
+            var icon5 = 'https://openweathermap.org/img/w/' + data.list[32].weather[0].icon + '.png';
+            $('#icon5').attr('src', icon5);
+            document.getElementById("temperature5").innerHTML = 'temperature: ' + Number(data.list[32].main.temp).toFixed(0) + "°F";
+            document.getElementById("wind5").innerHTML = 'wind: ' + Number(data.list[32].wind.speed) + " Mph";
+            document.getElementById("humidity5").innerHTML = 'humidity: ' + Number(data.list[32].main.humidity);
+            document.getElementById("date5").innerHTML = (data.list[32].dt_txt);
+        })
+
+})
